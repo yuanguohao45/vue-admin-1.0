c7cb3ec5520ab3f1ff1cb3b6c057b3f9debba3f5
baoxueernai <yuanguohao45@163.com>
2019-11-26 17:54:16 +0800

    构建项目、配置全局样式、路由、element-ui

---
+++ src/views/Login/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="login">
+    这是登录页
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+#login {
+  height: 100vh;
+  background-color: #344a5f;
+}
+</style>
