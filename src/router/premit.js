5a0c398b4487b715946c8853b5ae667b24fd8706
yuanguohao45 <yuanguohao45@163.com>
2019-12-09 22:44:13 +0800

    路由守卫、白名单

---
+++ src/router/premit.js
@@ -0,0 +1,16 @@
+import router from '@/router'
+import getToken from '@/utils/app'
+
+const whiteLinks = ['/login']
+
+router.beforeEach((to, from, next) => {
+  if (getToken()) {
+    next()
+  } else {
+    if (whiteLinks.indexOf(to.path)!==-1) {
+      next()
+    } else {
+      next('/login')
+    }
+  }
+})
