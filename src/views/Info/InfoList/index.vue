1161d70b2c2b9596a501d1e15842c226274b29ab
yuanguohao45 <yuanguohao45@163.com>
2019-12-28 14:29:10 +0800

    解决

--- src/views/Info/InfoList/index.vue
+++ src/views/Info/InfoList/index.vue
@@ -168,29 +168,7 @@ export default {
           showDialog.value = true;
           break;
         case "del":
-<<<<<<< HEAD
-          root
-            .$confirm("此操作将永久删除该文件, 是否继续?", "提示", {
-              confirmButtonText: "确定",
-              cancelButtonText: "取消",
-              type: "warning",
-              center: true
-            })
-            .then(() => {
-              root.$message({
-                type: "success",
-                message: "删除成功!"
-              });
-            })
-            .catch(() => {
-              root.$message({
-                type: "info",
-                message: "已取消删除"
-              });
-            });
-=======
           delMethod(row);
->>>>>>> 20cfb539bc6111b9a8b656ce2e95306fec224dbb
           break;
       }
     };
